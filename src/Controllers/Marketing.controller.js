a93859f598b7bc8b91095845250903740ed59f30
farhankhan0511 <farhankhan051104@gmail.com>
2026-01-09 00:43:03 +0530

    added more banners

--- src/Controllers/Marketing.controller.js
+++ src/Controllers/Marketing.controller.js
@@ -2,15 +2,35 @@ import { ApiResponse } from "../Utils/ApiResponse.js";
 import { asynchandler } from "../Utils/asynchandler.js";
 
 export const getBanners=asynchandler(async(req,res)=>{
-    res.status(200).json(new ApiResponse(200,{
-
-  "id":1,
-
-  "image_url": "https://cdn.bimakart.in/banners/exclusive_deal_1.png",
-
-  "web_url": "https://cdn.bimakart.in/banners/exclusive_deal_1.png",
-
-  "is_active": true
-
-},"Banners fetched successfully"  ))
+    res.status(200).json(new ApiResponse(200,[{
+        "id": 1,
+        "image_url": "https://user-policy-pdfs-prod.s3.ap-south-1.amazonaws.com/banner.jpeg",
+        "web_url": "https://bimakart.in/",
+        "is_active": true
+    },
+    {
+        "id": 2,
+        "image_url": "https://user-policy-pdfs-prod.s3.ap-south-1.amazonaws.com/banner.jpeg",
+        "web_url": "https://bimakart.in/",
+        "is_active": true
+    },
+    {
+        "id": 3,
+        "image_url": "https://user-policy-pdfs-prod.s3.ap-south-1.amazonaws.com/banner.jpeg",
+        "web_url": "https://bimakart.in/",
+        "is_active": true
+    },
+    {
+        "id": 4,
+        "image_url": "https://user-policy-pdfs-prod.s3.ap-south-1.amazonaws.com/banner.jpeg",
+        "web_url": "https://bimakart.in/",
+        "is_active": true
+    },
+    {
+        "id": 5,
+        "image_url": "https://user-policy-pdfs-prod.s3.ap-south-1.amazonaws.com/banner.jpeg",
+        "web_url": "https://bimakart.in/",
+        "is_active": true
+    },
+  ],"Banners fetched successfully"  ))
 })
